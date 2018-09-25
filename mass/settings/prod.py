5064c3703fc01981f3b89fe8b92f57bb1b196dd1
Marc Kerins <marc.kerins@bluvector.io>
2018-09-25 12:23:46 -0400

    Combine devel.py and test.py
    - during development and testing, FakeRedis and sqlite should be used so
      let's combine those to simplify things.
    - add the base ClamAV .cvd files so freshclam doesn't need to keep
      downloading them

--- mass/settings/prod.py
+++ mass/settings/prod.py
@@ -1,6 +1,5 @@
 from .base import *
 
-DEBUG = True
-TESTING = False
+DEBUG = False
 
 RQ_QUEUES['default']['HOST'] = 'redis'
--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# python manage.py makemigrations filestore
-# python manage.py migrate
-export DJANGO_SETTINGS_MODULE=mass.settings.test
 pip install flake8 coverage fakeredis
+export DJANGO_SETTINGS_MODULE=mass.settings.devel
+find {filestore,mass} -name "__pycache__" | xargs rm -rf
+rm -rf mass/db.sqlite3
+rm -rf files/*
+rm -rf */migrations/*
+rm -rf static/*
+cp etc/clamav/*.cvd /var/lib/clamav/
+chown -R clamav:clamav /var/lib/clamav
+python manage.py makemigrations filestore
+python manage.py migrate
 freshclam
 /etc/init.d/clamav-daemon start
-find {filestore,mass} -name "__pycache__" | xargs rm -rf
 python -m flake8 --max-line-length=119 filestore/ mass/
 rm -rf debug.log
 python -m coverage run --source=filestore --omit=filestore/tests/test_*.py,filestore/migrations/* manage.py test filestore/tests -v 2
