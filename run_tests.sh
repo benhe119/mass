600a6b494334a5d10d44f0a8661bdd2bac83e5b9
Marc Kerins <marc.kerins@bluvector.io>
2018-10-15 21:17:12 -0400

    Fix codecov
    run_tests.sh
    - added xml coverage report generation for codecov

--- run_tests.sh
+++ run_tests.sh
@@ -17,3 +17,4 @@ python -m flake8 --max-line-length=119 filestore/ mass/
 rm -rf debug.log
 python -m coverage run --source=filestore --omit=filestore/tests/test_*.py,filestore/migrations/* manage.py test filestore/tests -v 2
 python -m coverage report -m
+python -m coverage xml
